0d1f1b125427a3af5430ace7f472ba9a295220fe
Marcus Alves <marcus.b.alves@hotmail.com>
2020-05-15 00:12:24 -0300

    Adcionado classes Entidades.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public ICollection<ItemPedido> ItemPedidos { get; set; }
+
+    }
+}
